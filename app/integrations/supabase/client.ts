fe75c29374c85049818a8ef8611eb6a2dbf14696
alibcc <31526685+alibcc@users.noreply.github.com>
2026-04-02 20:54:59 +0200

    Update client.ts

--- app/integrations/supabase/client.ts
+++ app/integrations/supabase/client.ts
@@ -26,7 +26,7 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, 
     },
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: true,
   },
 });
 
